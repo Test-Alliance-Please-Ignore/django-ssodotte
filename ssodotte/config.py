c976a048ba2d185bbedb2b92473e9d94f3db5996
Tross Yvormes <tross@pleaseignore.com>
2020-04-30 14:01:11 -0400

    Set default OIDC_RP_SCOPES to "openid"

--- ssodotte/config.py
+++ ssodotte/config.py
@@ -4,6 +4,8 @@ OIDC_OP_USER_ENDPOINT = "https://sso.pleaseignore.com/auth/realms/auth-ng/protoc
 
 OIDC_RP_SIGN_ALGO = 'RS256'
 
+OIDC_RP_SCOPES = "openid"
+
 # This is derived from https://sso.pleaseignore.com/auth/realms/auth-ng/protocol/openid-connect/certs
 OIDC_RP_IDP_SIGN_KEY = """-----BEGIN RSA PUBLIC KEY-----
 MIIBCgKCAQEAqraRVf6zYu6k05Ml22xsUsUOZsmJP4Cqtq4CWut2HGwFFY49BmOV
