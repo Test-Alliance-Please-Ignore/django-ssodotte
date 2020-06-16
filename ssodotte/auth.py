661411a13463e1d349f8b7f6fa9561d931986515
rsgm <15-rsgm@users.noreply.cicd.pleaseignore.com>
2020-06-16 21:01:29 +0000

    Updated auth.py and middleware.py
    Auth.py
     - Removed unnecessary authenticate override
     - Changed time format to save token expirations as
    
    middleware.py
     - Moved into its own middleware, separated from SessionRefresh
     - Overrided is_refreshable_url, removing request method check

--- ssodotte/auth.py
+++ ssodotte/auth.py
@@ -1,10 +1,15 @@
+import logging
+import math
+import time
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 from mozilla_django_oidc.utils import import_from_settings
+
+LOGGER = logging.getLogger(__name__)
 
 
 class SsodotteBackend(OIDCAuthenticationBackend):
     def filter_users_by_claims(self, claims):
-        sub_id = claims.get('sub')
+        sub_id = claims.get("sub")
 
         if not sub_id:
             return self.UserModel.objects.none()
@@ -12,17 +17,41 @@ class SsodotteBackend(OIDCAuthenticationBackend):
         return self.UserModel.objects.filter(username=sub_id)
 
     def verify_claims(self, claims):
-        """Verify the provided claims to decide if authentication should be allowed."""
+        """
+        Verify the provided claims to decide if authentication should be allowed.
+        """
         return "sub" in claims
 
     def create_user(self, claims):
-        sub_id = claims.get('sub')
+        sub_id = claims.get("sub")
 
         if not sub_id:
-            raise ValueError('No subject ID found in claim')
+            raise ValueError("No subject ID found in claim")
 
         return self.UserModel.objects.create_user(sub_id)
 
     def get_username(self, claims):
         # this method is only used in create_user, but we overrode that, so it should be unused.
-        raise Exception('This should not have been called, something went wrong. mozilla-django-oidc probably updated and broke something')
+        raise Exception(
+            "This should not have been called, something went wrong. mozilla-django-oidc probably updated and broke something"
+        )
+
+    def get_token(self, payload):
+        """
+        Return token object as a dictionary.
+        """
+
+        token_info = super(SsodotteBackend, self).get_token(payload)
+
+        if import_from_settings("OIDC_STORE_REFRESH_TOKENS", False):
+            session = self.request.session
+
+            session["oidc_refresh_token"] = token_info.get("refresh_token")
+            session["oidc_refresh_token_expiration"] = math.floor(
+                time.time() + token_info.get("refresh_expires_in")
+            )
+            session["oidc_access_token_expiration"] = math.floor(
+                time.time() + token_info.get("expires_in")
+            )
+
+        return token_info
---
+++ ssodotte/middleware.py
@@ -0,0 +1,148 @@
+import logging
+import time
+import json
+
+from requests import RequestException
+from django.contrib.auth import BACKEND_SESSION_KEY, load_backend, logout, login
+from django.core.exceptions import SuspiciousOperation
+from mozilla_django_oidc.auth import OIDCAuthenticationBackend
+from mozilla_django_oidc.middleware import SessionRefresh
+from mozilla_django_oidc.utils import import_from_settings, is_authenticated
+from josepy.b64 import b64decode
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class TokenRefreshMiddleware(SessionRefresh):
+    """
+    Refreshes access and ID tokens after expiration.
+
+    For users authenticated with the OIDC RP, verify tokens are still valid and
+    if not, refresh tokens without user interruption.
+
+    """
+
+    def get_backend_instance(self, backend_path):
+        if not backend_path:
+            return None
+
+        return load_backend(backend_path)
+
+    def is_refreshable_url(self, request, backend):
+        """
+        Takes a request and returns whether it triggers a refresh examination
+
+        :arg HttpRequest request:
+
+        :returns: boolean
+
+        """
+        # Do not attempt to refresh the session if the OIDC backend is not used
+        if not isinstance(backend, OIDCAuthenticationBackend):
+            return False
+
+        return (
+            # unlike SessionRefresh, don't check request method
+            is_authenticated(request.user)
+            and request.path not in self.exempt_urls
+        )
+
+    def get_jwt_payload_no_validation(self, token):
+        payload_data = token.split(".")[1]
+        return json.loads(b64decode(payload_data))
+
+    def logout(self, request):
+        """Remove OIDC session variables and log out user."""
+        logout(request)
+        for variable in [
+            "oidc_refresh_token",
+            "oidc_refresh_token_expiration",
+            "oidc_access_token_expiration",
+            "oidc_access_token",
+            "oidc_id_token",
+        ]:
+            try:
+                del request.session[variable]
+            except KeyError:
+                pass
+
+    def process_request(self, request):
+        if not import_from_settings("OIDC_STORE_REFRESH_TOKENS", False):
+            LOGGER.debug("OIDC_STORE_REFRESH_TOKENS isn't on")
+            return
+
+        backend_path = request.session.get(BACKEND_SESSION_KEY)
+        backend = self.get_backend_instance(backend_path)
+
+        if not self.is_refreshable_url(request, backend):
+            LOGGER.debug("request is not refreshable")
+            return
+
+        LOGGER.debug("checking tokens")
+        now = time.time() + 15  # add 15s, so we don't expire while making a call
+        refresh_token_expiration = request.session.get(
+            "oidc_refresh_token_expiration", 0
+        )
+        access_token_expiration = request.session.get("oidc_access_token_expiration", 0)
+
+        # Log the user out if the refresh token is expired (we can't refresh
+        # their token)
+        if now >= refresh_token_expiration:
+            self.logout(request)
+            return
+
+        if access_token_expiration < now < refresh_token_expiration:
+            # try to refresh expired access token with refresh token
+            LOGGER.debug(
+                "access token expired, refreshing. token expiries: %s",
+                [access_token_expiration - now, refresh_token_expiration - now],
+            )
+
+            token_payload = {
+                "grant_type": "refresh_token",
+                "refresh_token": request.session["oidc_refresh_token"],
+                "client_id": import_from_settings("OIDC_RP_CLIENT_ID"),
+                "client_secret": import_from_settings("OIDC_RP_CLIENT_SECRET"),
+            }
+
+            # Call the OIDCAuthenticationBackend to get/store the new tokens
+            # and validate access
+            backend.request = request
+            try:
+                token_info = backend.get_token(token_payload)
+            except RequestException:
+                LOGGER.warning("Could not refresh token", exc_info=True)
+                self.logout(request)
+                return
+
+            id_token = token_info.get("id_token")
+            access_token = token_info.get("access_token")
+
+            # We can't actually check the nonce here (it's the same as the one
+            # used on initial login), so just fetch the nonce in the token and
+            # cause the comparison to be a no-op
+            nonce = self.get_jwt_payload_no_validation(id_token).get("nonce")
+            payload = backend.verify_token(id_token, nonce=nonce)
+
+            user = None
+            if payload:
+                backend.store_tokens(access_token, id_token)
+                try:
+                    user = backend.get_or_create_user(access_token, id_token, payload)
+                except SuspiciousOperation:
+                    LOGGER.warning(
+                        "Could not get or create user during token refresh",
+                        exc_info=True,
+                    )
+
+            if not user or request.user.id != user.id:
+                self.logout(request)
+        else:
+            # The access token is still valid, so we don't have to do anything.
+            LOGGER.debug(
+                "tokens are still valid, not auto refreshing, (%s, %s > %s)",
+                refresh_token_expiration,
+                access_token_expiration,
+                now,
+            )
